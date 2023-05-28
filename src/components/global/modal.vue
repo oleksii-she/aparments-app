0f9a849798b616f2e950026c57669381f1637569
Your Name <vazerius@gmail.com>
2023-05-28 22:35:05 +0300

    add project

---
+++ src/components/global/modal.vue
@@ -0,0 +1,97 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+import { onMounted, onBeforeUnmount } from 'vue'
+const props = defineProps({
+  toggleModal: {
+    type: Boolean,
+    required: true
+  },
+  hideDialog: {
+    type: Function,
+    required: false
+  }
+})
+
+const handleEscKey = (event) => {
+  if (event.keyCode === 27) {
+    props.hideDialog()
+  }
+}
+
+onMounted(() => {
+  window.addEventListener('keydown', handleEscKey)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('keydown', handleEscKey)
+})
+// WatchEffect(() => {
+//   //   if (props.toggleModal) {
+//   //     document.body.style.overflow = 'hidden'
+//   //   }
+// })
+
+// const onClose = (e) => {
+//   console.log(e.target)
+//   if (e.target === e.currentTarget) {
+//     // document.body.style.overflow = ''
+//     // props.toggleModal()
+//   }
+//   //   Overlay()
+// }
+</script>
+
+<template>
+  <div :class="toggleModal ? 'show_modal Overlay' : 'isHidden'" @click.stop="hideDialog">
+    <div class="modal" @click.stop>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.Overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1200;
+  visibility: hidden;
+  background: rgba(17, 17, 17, 0.6);
+  backdrop-filter: blur(10px);
+  transition: 0.5s;
+  &.isHidden {
+    opacity: 0;
+  }
+  &.show_modal {
+    opacity: 1;
+    visibility: visible;
+  }
+}
+.modal {
+  background: #ffffff;
+  box-shadow: 7px 4px 14px rgba(0, 0, 0, 0.11);
+  border-radius: 40px;
+  /* transform: scale(0.5); */
+  transition: 0.4s all;
+  margin: auto;
+
+  padding: 20px;
+  &.show {
+    transform: scale(1);
+    max-height: 90%;
+    overflow: auto;
+  }
+  &.show::-webkit-scrollbar {
+    display: none;
+  }
+  &.hidden {
+    transform: scale(0.5);
+    transition: 0.4s all;
+  }
+}
+</style>
