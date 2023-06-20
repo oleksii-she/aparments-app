a2bd1ae7d2bba9ef69d933d3ad172d6527d0ef19
Your Name <vazerius@gmail.com>
2023-06-20 13:23:58 +0300

    update error

--- src/pages/ErrorPage.vue
+++ src/pages/ErrorPage.vue
@@ -1,7 +1,20 @@
 <template>
-  <div class="about">
-    <h1>Error</h1>
+  <div class="container">
+    <div class="wrapper">
+      <h1>Error 404</h1>
+      <h2><RouterLink :to="{ name: 'apartments' }"> Go to Home</RouterLink></h2>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 80px;
+  height: 86vh;
+  margin: 0 auto;
+}
+</style>
